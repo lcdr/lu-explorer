64f233f0716074b0e570d7d009ba91f726b27b5b
lcdr <lcdr@users.noreply.github.com>
2023-11-04 20:41:33 +0100

    Use SQL query API in reward codes page

--- src/app/objects/reward-codes/reward-codes.component.ts
+++ src/app/objects/reward-codes/reward-codes.component.ts
@@ -1,49 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { DB_RewardCodes } from '../../../defs/cdclient';
-import { Locale_RewardCodes } from '../../../defs/locale';
-import { LuCoreDataService } from '../../util/services/lu-core-data.service';
-import { ObjectsByComponentComponent } from '../by-component/by-component.component';
-
-type RewardCodeDict = { gate_version: string, codes: DB_RewardCodes[] }[];
-
-function codeSort(a: DB_RewardCodes, b: DB_RewardCodes) {
-  return a.code.localeCompare(b.code);
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lux-reward-codes',
   templateUrl: './reward-codes.component.html',
   styleUrls: ['./reward-codes.component.css']
 })
-export class RewardCodesComponent implements OnInit {
-
-  $code_groups: Observable<RewardCodeDict>;
-  $loc: Observable<{[key: string]: Locale_RewardCodes}>;
-
-  constructor(private luCoreData: LuCoreDataService) { }
-
-  ngOnInit(): void {
-    this.$code_groups = this.luCoreData.getTableEntry("RewardCodes", "all").pipe(map(list => {
-      let dict: { [key: string]: DB_RewardCodes[] } = {};
-      for (let code of list) {
-        let gate = code.gate_version || '';
-        if (!dict.hasOwnProperty(gate)) {
-          dict[gate] = [];
-        };
-        dict[gate].push(code);
-      }
-      return Object.keys(dict).sort().map(gate => {
-        return { gate_version: gate, codes: dict[gate].sort(codeSort) }
-      });
-    }));
-    this.$loc = this.luCoreData.getLocaleSubtree("RewardCodes");
-  }
-
-  loc(key: number): Observable<Locale_RewardCodes> {
-    const str = String(key);
-    return this.$loc.pipe(map(all => all[str]));
-  }
-
-}
+export class RewardCodesComponent {}
