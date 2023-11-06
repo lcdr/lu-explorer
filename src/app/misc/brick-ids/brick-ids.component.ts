55b7b3b26336318cc77c5a19070ca175ea465079
lcdr <lcdr@users.noreply.github.com>
2023-11-06 21:13:35 +0100

    Use SQL query API in brick IDs page
    Also adds sort direction toggling, the ability to sort by name, and slightly adjusts column layout to be more compact.

--- src/app/misc/brick-ids/brick-ids.component.ts
+++ src/app/misc/brick-ids/brick-ids.component.ts
@@ -1,46 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { DB_BrickIDTable } from '../../../defs/cdclient';
-import { LuCoreDataService } from '../../util/services/lu-core-data.service';
 
 @Component({
   selector: 'app-brick-ids',
   templateUrl: './brick-ids.component.html',
   styleUrls: ['./brick-ids.component.css']
 })
-export class BrickIdsComponent implements OnInit {
-
-  $table: Observable<DB_BrickIDTable[]>;
+export class BrickIdsComponent {
   sort: string = 'LEGOBrickID';
   limit: number = 50;
-  objects: any = {};
-  $page: Observable<number>;
+  ascending: boolean = true;
+  page: number = 0;
 
-  constructor(private coreData: LuCoreDataService, private route: ActivatedRoute) {
-    this.$page = this.route.params.pipe(map(p => Number(p.page || 0)));
-  }
-
-  ngOnInit() {
-    this.getTable();
-  }
-
-  /*
-  onLocaleTableObjects(table: any) {
-    table.pages.forEach(page => this.luLocale.getLocalePage("Objects", page).subscribe(this.onLocalTableObjectsPage.bind(this)))
-  }
-
-  onLocalTableObjectsPage(page: any) {
-    Object.assign(this.objects, page);
-  }
-  */
-
-  getTable(): void {
-    this.$table = this.coreData.getFullTable("BrickIDTable");
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(p => this.page = Number(p.page || 0));
   }
 
   setSort(key: string) {
-    this.sort = key;
+    if (this.sort != key) {
+      this.sort = key;
+      this.ascending = true;
+    } else {
+      this.ascending  = !this.ascending;
+    }
   }
 }
