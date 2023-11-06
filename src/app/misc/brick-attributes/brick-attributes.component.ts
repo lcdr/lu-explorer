8cc9a1cb1a6f1c4ea1b82af8fa63b8fae5ab5ea4
lcdr <lcdr@users.noreply.github.com>
2023-11-06 18:52:59 +0100

    Use SQL query API in brick attributes page

--- src/app/misc/brick-attributes/brick-attributes.component.ts
+++ src/app/misc/brick-attributes/brick-attributes.component.ts
@@ -1,23 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { LuCoreDataService } from '../../services';
-import { DB_brickAttributes } from '../../../defs/cdclient';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-brick-attributes',
   templateUrl: './brick-attributes.component.html',
   styleUrls: ['./brick-attributes.component.css']
 })
-export class BrickAttributesComponent implements OnInit {
-
-  table: Observable<DB_brickAttributes[]>;
-
-  constructor(
-    private luCoreData: LuCoreDataService) { }
-
-  ngOnInit() {
-    this.table = this.luCoreData.getFullTable('brickAttributes');
-  }
-
-}
+export class BrickAttributesComponent {}
