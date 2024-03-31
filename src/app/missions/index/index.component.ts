524d34138e4ca47fb556e4f26cca756af5b179e0
lcdr <lcdr@users.noreply.github.com>
2024-03-31 17:35:57 +0200

    Use SQL query API in mission index view, redesign it for better space usage

--- src/app/missions/index/index.component.ts
+++ src/app/missions/index/index.component.ts
@@ -1,21 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LuCoreDataService } from '../../util/services/lu-core-data.service';
+import { Component } from '@angular/core';
 
-type MissionTypes = { [key: string]: string[] };
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class MissionIndexComponent implements OnInit {
-
-  missions: any = {};
-  $mission_types: Observable<MissionTypes>;
-
-  constructor(private luCoreDataService: LuCoreDataService) { }
-
-  ngOnInit() {
-    this.$mission_types = this.luCoreDataService.getRev<MissionTypes>('mission_types')
-  }
-}
+export class MissionIndexComponent {}
