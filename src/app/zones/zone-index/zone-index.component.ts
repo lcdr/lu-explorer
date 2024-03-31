5d360f38fbcc53bd18fd8f510eb8995babf53af2
lcdr <lcdr@users.noreply.github.com>
2024-03-31 22:46:00 +0200

    Use SQL query API in zone index view, redesign to include signage, make better use of available space

--- src/app/zones/zone-index/zone-index.component.ts
+++ src/app/zones/zone-index/zone-index.component.ts
@@ -1,22 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Locale_ZoneTable } from '../../../defs/locale';
-import { LuCoreDataService } from '../../util/services/lu-core-data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-zone-index',
   templateUrl: './zone-index.component.html',
   styleUrls: ['./zone-index.component.css']
 })
-export class ZoneIndexComponent implements OnInit {
-
-  $zones: Observable<{ [key: string]: Locale_ZoneTable }>;
-
-  constructor(
-    private luCoreData: LuCoreDataService,
-  ) { }
-
-  ngOnInit() {
-    this.$zones = this.luCoreData.getLocaleSubtree<Locale_ZoneTable>('ZoneTable');
-  }
-}
+export class ZoneIndexComponent {}
